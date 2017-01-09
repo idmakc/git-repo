d5b8535ba2277121e0483a5924650926d896a991
maks <maks@it-14.motiv.net>
2017-01-09 15:30:15 +0500

    mypytest3-7-1

---
+++ 1.py
@@ -0,0 +1,24 @@
+a=int(input())
+b=[]
+for k in range(a):
+	b.append(input().split(';'))
+print(b)
+
+plays=0
+win=0
+draw=0
+lose=0
+pts=0
+for i in range(a):
+	if int(b[i][1])>int(b[i][3]):
+		tot.append
+
+com=set()
+for i in range(a):
+	com.add(b[i][0])
+print(com)	
+
+
+
+
+
